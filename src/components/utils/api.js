eaa5e6f89fea72e611ffdb39b200d92f97eddaab
mohdriyaz0807 <mohdriyaz0807@gmail.com>
2023-06-01 14:15:17 +0530

    API Path config

--- src/components/utils/api.js
+++ src/components/utils/api.js
@@ -1,2 +1,2 @@
-// export const API_PATH = "https://reinvented-peat-fibula.glitch.me"
-export const API_PATH = "http://localhost:4000"
+export const API_PATH = "https://reinvented-peat-fibula.glitch.me"
+// export const API_PATH = "http://localhost:4000"
