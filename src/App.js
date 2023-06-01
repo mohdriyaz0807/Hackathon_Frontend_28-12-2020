8a124ea4904b97283a3c1d5ecdee4dc158f797cf
mohdriyaz0807 <mohdriyaz0807@gmail.com>
2023-06-01 17:32:55 +0530

    redirect fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Header from "./components/Header/Header";
 import Error404 from "./components/Error-404/404";
@@ -11,29 +16,46 @@ import { useEffect } from "react";
 import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [refresh,setrefresh] = useState(false)
-  const [cart, setCart] = useLocalStorage('cart', [])
-  const [open, setOpen] = useState(false)
+  const [refresh, setrefresh] = useState(false);
+  const [cart, setCart] = useLocalStorage("cart", []);
+  const [open, setOpen] = useState(false);
 
-  useEffect(()=>{
-    setrefresh(!refresh)
-  },[cart])
+  const token = window.localStorage.getItem("token");
+
+
+  useEffect(() => {
+    setrefresh(!refresh);
+  }, [cart]);
 
   return (
     <Router>
-      <Header cart={cart} openLogin={() => setOpen(true)}/>
-      <Login open={open} closeModal={() => setOpen(false)}/>
+      <Header cart={cart} openLogin={() => setOpen(true)} />
+      <Login open={open} closeModal={() => setOpen(false)} />
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/Dashboard" />} />
         <Route exact path="/Dashboard">
           <Dashboard cart={cart} setCart={setCart} />
         </Route>
-        <Route exact path="/Cart">
-          <Cart cart={cart} setCart={setCart} openLogin={() => setOpen(true)}/>
-        </Route>
-        <Route exact path="/Orders">
-          <Orders />
-        </Route>
+        <Route
+          exact
+          path="/Cart"
+          render={() =>
+            token ? (
+              <Cart
+                cart={cart}
+                setCart={setCart}
+                openLogin={() => setOpen(true)}
+              />
+            ) : (
+              <Redirect to="/" />
+            )
+          }
+        />
+        <Route
+          exact
+          path="/Orders"
+          render={() => (token ? <Orders /> : <Redirect to="/" />)}
+        />
         <Route path="*">
           <Error404 />
         </Route>
